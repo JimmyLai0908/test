f8f09de982cc53fb5bafa54053394b8eeb24eca8
JimmyLai0908 <jinylimit@gmail.com>
2017-04-14 17:10:36 +0800

    Update arduino101_serial_extension.js

--- arduino101_serial_extension.js
+++ arduino101_serial_extension.js
@@ -307,7 +307,7 @@
   };
 
   var blocks = [
-    [' ', 'set pin %d.digitalOutputs %m.outputs', 'digitalWrite', 13, 'on'],
+    [' ', 'set DigitalPin %d.digitalOutputs %m.outputs', 'digitalWrite', 13, 'on'],
     [' ', 'set pin %d.analogOutputs to %n%', 'analogWrite', 9, 100],
     ['h', 'when pin %d.digitalInputs is %m.outputs', 'whenDigitalRead', 9, 'on'],
     ['b', 'pin %d.digitalInputs on?', 'digitalRead', 9],
